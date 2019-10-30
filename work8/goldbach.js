4a11aceb6925737b073ef7dc8a89ffbceeb3e4e2
chunchunden <chunchunden@qq.com>
2019-10-30 12:19:42 +0800

    work9

---
+++ work8/goldbach.js
@@ -0,0 +1,12 @@
+function goldbach(suShu){
+    let goldbach = document.getElementById('goldbach')
+    let odd = document.getElementById('odd')
+    if(isNaN(odd)){
+        alert('请输入一个大于2的偶数')
+        return false
+    }
+    goldbach.value = suShu(odd)
+}
+function goldbach(odd){
+    if
+}
