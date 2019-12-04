237f9cf31683ed9b2e61e27a9ecd4660efab6a60
chunchunden <chunchunden@qq.com>
2019-12-04 21:38:37 +0800

    work13

--- work13/index.js
+++ work13/index.js
@@ -4,7 +4,7 @@ var elements = [
     {tag: 'input', text: '班级:', attr: {type: 'text',name:'classid'}},
     {tag: 'input', text: '性别:', attr: {type: 'radio',name:'gender'},
     option: {m: '男', w: '女'}},
-    {tag: 'input', text: '爱好：', attr: {type: 'radio',name:'hobby[]'},
+    {tag: 'input', text: '爱好：', attr: {type: 'checkbox',name:'hobby[]'},
     option: {swimming: '游泳', reading:'读书', running:'跑步'}},
     {tag: 'select', text: '住址:', attr: {name:'area'},
     option: {'': '--请选择--',bj:'北京',sh:'上海',sz:'深圳'}},
